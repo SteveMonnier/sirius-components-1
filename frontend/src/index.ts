4a118e75b505730d3251d0e596a894ce719a727e
Pierre-Charles David <pierre-charles.david@obeo.fr>
2020-11-27 15:26:49 +0100

    [113] Do not provide a default ApolloClient
    It is the responsibility of each application to build a proper
    ApolloClient and provide it via ApolloProvider.
    
    sirius-web will contain a sample client that can be used as an
    example/starting point.
    
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -14,7 +14,6 @@
 export * from 'auth/useAuth';
 export * from 'capabilities/CapabilitiesProvider';
 export * from 'capabilities/useCapabilities';
-export * from 'common/ApolloGraphQLClient';
 export * from 'common/GraphQLClient';
 export * from 'common/GraphQLHooks';
 export * from 'common/BrandingContext';
